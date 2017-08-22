3b79a850eb0c58bba20a96a8618e256a9c72eaf9
Jonathan <jonathanmosesman@gmail.com>
2017-08-21 20:42:04 -0500

    Change to spanish welcome

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "Hello, world!"
+    render html: "¡Hola, señor!"
   end
 
   def goodbye
     render html: "Goodbye, world"
   end
-  
+
 end
